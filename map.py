5f69970fddc37be2062a192135256ba5a60424c6
Mark Abraham <65253145+mabrahamdevops@users.noreply.github.com>
2020-05-26 12:10:04 -0400

    Update flask code

--- map.py
+++ map.py
@@ -30,13 +30,13 @@ import pandas
 from folium import IFrame
 from flask import Flask, render_template
 
-app = Flask(__map__)
+app = Flask (__name__)
 
 @app.route("/")
 def home():
     return render_template('Map_html_popup.html')
 
-if __map__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
 
 
